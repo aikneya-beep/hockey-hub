4abb45ce70d3c3e89ef4b4ccc1dbf1d82bab5570
aikneya-beep <aikneya@gmail.com>
2026-09-17 14:57:23 +0300

    Wire environment composition patch v0.50.1

---
+++ app_v501.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import app_v50
+import app_v05 as core
+import ui_patch_v501  # rebalances personal hockey environment
+
+core.app.version = "0.50.1"
+
+app = core.app
