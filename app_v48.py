6bcb152c5058ee32002f4baf66324fefc579b684
aikneya-beep <aikneya@gmail.com>
2026-09-17 12:35:41 +0300

    Register Hockey Closet in v0.48

---
+++ app_v48.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import app_v47
+import app_v05 as core
+import closet_v48  # registers /my-hockey/closet and patches personal navigation
+
+core.app.version = "0.48.0"
+
+app = core.app
