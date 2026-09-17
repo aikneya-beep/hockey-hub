d94630393e3d41e9292ee6aa2aee402f82a5eed8
aikneya-beep <aikneya@gmail.com>
2026-09-17 15:35:18 +0300

    Wire v0.51 UX cleanup

---
+++ app_v51.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import app_v502
+import app_v05 as core
+import ui_cleanup_v51  # UX hierarchy cleanup across main screens
+
+core.app.version = "0.51.0"
+
+app = core.app
